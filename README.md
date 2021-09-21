29543724374398b0fcebc00c4d24baf5c0d1a455
kopinmuli <samulikoppi@gmail.com>
2021-09-21 12:46:55 +0300

    päivitettiin main branchin kanssa yhteen

---
+++ README.md
@@ -0,0 +1,7 @@
+# Kimppakyyti-Sovellus-NAYTTO
+Luodaan Kimppakyyti-sovellus Ohjelmoinnin näytöksi
+
+# Toimintakaavio
+
+
+![asd](https://user-images.githubusercontent.com/77320275/134124826-05bea29f-3b50-4f5d-b43f-edd6b96ba13a.png)
---
+++ js/main.js
@@ -0,0 +1,59 @@
+var kayttajatunnus;
+
+function kirjaudu() {
+  var kayttajanimi = document.getElementById("kayttajanimi").value;
+  var salasana = document.getElementById("salasana").value;
+  var osuma = -1;
+
+  var tunnusArray = ["samuli", "niko", "admin"];
+  var salasanaArray = ["samuli", "niko", "admin"];
+  var nimiArray = ["Samuli Koppi","Niko Eklöf","Ylläpitäjä"];
+
+  for (var i=0; i <tunnusArray.length; i++) {
+      if ((kayttajanimi == tunnusArray[i]) && (salasana == salasanaArray[i])) {
+          osuma = i;
+          break;
+      }
+  }
+  if (osuma != -1) {
+      kayttajatunnus=document.getElementById("kayttajanimi").value;
+      suljeTausta();
+      document.getElementById("kirjautunutkayttaja").style.display ="block";
+      document.getElementById("kirjautunutkayttaja").innerHTML = "Tervetuloa, "+ nimiArray[osuma];
+  }
+  else {
+    document.getElementById("kirjautuminen").style.display ="none";
+    document.getElementById("kirjautuminenError").style.display ="block";
+  }
+}
+
+function suljeError() {
+  document.getElementById("kirjautuminen").style.display ="block";
+  document.getElementById("kirjautuminenError").style.display ="none";
+}
+
+function suljeTausta() {
+  document.getElementById("fullscreenkuva").style.display ="none";
+  document.getElementById("sisalto").style.display ="block";
+  document.getElementById("navigointi").style.display ="block";
+  document.getElementById("etsikyytia").style.display ="block";
+}
+
+
+function etsiKyytia() {
+  document.getElementById("etsikyytia").style.display ="block";
+  document.getElementById("tarjoakyytia").style.display ="none";
+  document.getElementById("ohjeetsivu").style.display ="none";
+}
+
+function tarjoaKyytia() {
+  document.getElementById("etsikyytia").style.display ="none";
+  document.getElementById("tarjoakyytia").style.display ="block";
+  document.getElementById("ohjeetsivu").style.display ="none";
+}
+
+function ohjeet() {
+  document.getElementById("etsikyytia").style.display ="none";
+  document.getElementById("tarjoakyytia").style.display ="none";
+  document.getElementById("ohjeetsivu").style.display ="block";
+}
