39e220e8bc5cf3ff54299ae75b0380dc67f87b41
Niko Eklöf <80674961+nikoeklof@users.noreply.github.com>
2021-10-06 09:05:46 +0300

    reitin poistamisen bugien korjaus

--- js/kartta.js
+++ js/kartta.js
@@ -64,10 +64,10 @@ function naytaKaikkiKyydit() {
 }
 
 function lataaKyydit() {
-    if(reittiJSON.length > 0){
+    if (reittiJSON.length > 0) {
         reittiID = reittiJSON.length
     }
-    
+
     for (let i = 0; i < reittiJSON.length; i++) {
         setTimeout(() => {
             reitti.push({
@@ -108,10 +108,10 @@ function lataaKyydit() {
                             if (!reitti[thisID].valittu) {
                                 reitti[thisID].router._line.getLayers()[reitti[thisID].router._line.getLayers().length - 1]._path.setAttribute("stroke", "green")
                                 reitti[thisID].valittu = true
-                                
+
                             } else {
                                 reitti[thisID].router._line.getLayers()[reitti[thisID].router._line.getLayers().length - 1]._path.setAttribute("stroke", "red")
-                                
+
                             }
                             if (reitti[thisID].kayttajanimi != kirjautunut || kirjautunut == 'Ylläpitäjä') {
                                 document.getElementById('poistareittibutton' + thisID).style.display = "none"
@@ -277,7 +277,7 @@ function luoReitti(lahto, maaranpaa, reittiID, kayttajanimi, paivamaara, lahtoAi
                         reitti[i].router._line.getLayers()[reitti[i].router._line.getLayers().length - 1]._path.setAttribute("stroke", "red")
                         reitti[i].valittu = false
                     }
-                    if (!reitti[thisID].valittu ) {
+                    if (!reitti[thisID].valittu) {
                         reitti[thisID].router._line.getLayers()[reitti[thisID].router._line.getLayers().length - 1]._path.setAttribute("stroke", "green")
                         reitti[thisID].valittu = true
                     } else {
@@ -321,29 +321,29 @@ function paivitaReittiID() {
 
 
 function tallennaReitit() {
-    if (reitti.length != 0) {
-        for (let i = 0; i < reitti.length; i++) {
-            if (reitti[i] != undefined) {
-                var Reittistring = {
-                    id: reitti[i].id,
-                    kayttajanimi: reitti[i].kayttajanimi,
-                    paivamaara: reitti[i].paivamaara,
-                    lahtoaika: reitti[i].lahtoaika,
-                    lahto: reitti[i].lahto,
-                    maaranpaa: reitti[i].maaranpaa,
-                    yhteystieto: reitti[i].yhteystieto,
-                    lisaTiedot: reitti[i].lisaTiedot,
-                    routerWaypoints: [reitti[i].router.getWaypoints()[0].latLng, reitti[i].router.getWaypoints()[1].latLng],
+    localStorage.removeItem('tallennetutReitit')
+    reittiJSON = []
 
-                }
-                reittiJSON.push(Reittistring)
+    for (let i = 0; i < reitti.length; i++) {
+        if (reitti[i] != undefined) {
+            var Reittistring = {
+                id: reitti[i].id,
+                kayttajanimi: reitti[i].kayttajanimi,
+                paivamaara: reitti[i].paivamaara,
+                lahtoaika: reitti[i].lahtoaika,
+                lahto: reitti[i].lahto,
+                maaranpaa: reitti[i].maaranpaa,
+                yhteystieto: reitti[i].yhteystieto,
+                lisaTiedot: reitti[i].lisaTiedot,
+                routerWaypoints: [reitti[i].router.getWaypoints()[0].latLng, reitti[i].router.getWaypoints()[1].latLng],
+
             }
+            reittiJSON.push(Reittistring)
         }
-    } else {
-        localStorage.removeItem('tallennetutReitit')
-        reittiJSON = []
-        return
     }
+
+
+
 
 
     localStorage.setItem('tallennetutReitit', JSON.stringify(reittiJSON))
